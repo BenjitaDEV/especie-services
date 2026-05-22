fa8e957ba7082d59742cc1a642ec9cd4f3ba8077
BenjitaDEV <benjamin.cespedes2708@gmail.com>
2026-05-22 18:05:48 -0400

    especie

---
+++ src/test/java/com/caleta/especie/EspecieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caleta.especie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspecieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
